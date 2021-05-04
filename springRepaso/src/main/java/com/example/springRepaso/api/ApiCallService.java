7d898c40366605e0bb92e546b0ae71c69991b0d8
NicolasJbo <57155671+NicolasJbo@users.noreply.github.com>
2021-05-03 21:59:56 -0300

    CircuitBreaker

--- springRepaso/src/main/java/com/example/springRepaso/api/ApiCallService.java
+++ springRepaso/src/main/java/com/example/springRepaso/api/ApiCallService.java
@@ -1,6 +1,11 @@
 package com.example.springRepaso.api;
 
 import com.google.gson.Gson;
+
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -8,11 +13,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.rmi.RemoteException;
 
 @Service
+@Slf4j
 public class ApiCallService {
 
+    @CircuitBreaker(name = "chuckNorris",fallbackMethod = "fallback")
     public  ChuckNorrisResponse getChuckNorris() throws IOException, InterruptedException {
+
+        if(RandomUtils.nextBoolean())
+        {
+        throw new RemoteException("");
+        }
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random"))
                 .header("accept", "application/json")
@@ -23,7 +37,10 @@ public class ApiCallService {
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 
         return new Gson().fromJson(response.body(),ChuckNorrisResponse.class);
-
-
     }
+    public  ChuckNorrisResponse fallback(final Throwable t ){
+        log.info("Fallback cause, {}",t.toString());
+        return ChuckNorrisResponse.builder().build();
+    }
+
 }
